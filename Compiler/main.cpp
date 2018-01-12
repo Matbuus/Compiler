44e15437cef663c89d8206a81891bb28ed525a49
Malek <attiamalek2@gmail.com>
2018-01-12 16:36:08 +0100

    Initial Commit

---
+++ Compiler/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Compiler
+//
+//  Created by Malek Attia on 1/12/18.
+//  Copyright © 2018 Malek Attia. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
